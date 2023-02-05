e1d502529bd97269780f76394db2310eab3ce128
prashant <prashantmahale18@gmail.com>
2023-02-05 14:45:58 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-------typing animation---*/
+var typed = new Typed(".typing",{
+    strings:["Student","Python Devloper","YouTuber","Blogger"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
